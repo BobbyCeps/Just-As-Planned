2a9c975dab138c76293ed73f2b481a21bc1064f5
BobbyCeps <BobbyCeps@gmail.com>
2015-12-16 16:24:04 +0100

    test temps

--- load.js
+++ load.js
@@ -1,12 +1,7 @@
 var dateNav = new Date();
-
 function load_all() {
   pokedex(); /* apelle la fonction pokedex */
-
-  dateNav = Date.now();
-
-window.alert(dateNav);
-
+  dateNav = Date.now(); /* apelle l'objet dateNav */
 
     /* pour fermer les pages lors du retour a l'acceuil */
   document.getElementById("content_details").style.display = "none";
@@ -15,4 +10,22 @@ window.alert(dateNav);
   document.getElementById("content_boutique").style.display='none';
     /* pour fermer le pokedex a l'acceuil */
   document.getElementById("content_pokedex").style.display='none';
+
+
+  /* on créé les pokemons */
+  var magicarpe = JSON.parse(localStorage.getItem("Magicarpe"));
+    if (!magicarpe) { var magicarpe = new Pokemon("Magicarpe", "", 129,"male",0,0,0,0, "images/magikarp.gif", dateNav);}
+  var pikachu = JSON.parse(localStorage.getItem("Pikachu"));
+  if (!pikachu) {
+    var pikachu = new Pokemon("Pikachu", "", 25, "male", 0,0,0,0, "images/pikachu.gif", dateNav);
+  }
+  var porygon = new Pokemon("Porygon", "", 137, "male", 0,0,0,0, "images/porygon.gif", dateNav);
+  /* on affiche magicarpe */
+  document.getElementById("sprite").src = magicarpe.image;
+  document.getElementById("Nom").innerHTML = magicarpe.nom ;
+  document.getElementById("progress_santé").value = magicarpe.santé
+  document.getElementById("progress_humeur").value = magicarpe.humeur
+  document.getElementById("progress_faim").value = magicarpe.faim
+  document.getElementById("progress_ennui").value = magicarpe.ennui
+
 } /* fin fonction load_all */
--- pokemon.js
+++ pokemon.js
@@ -8,17 +8,14 @@ function Pokemon (nom, surnom, numero, sexe, santé, humeur, faim, ennui, image,
   this.faim = faim;
   this.ennui = ennui;
   this.image = image;
-  this.date = new Date();
+  this.date = Date.now();
+  var dateDiff = this.date - dateNav;
 
+  this.update = function () {
+   this.santé--;
+   this.ennui++;
+   this.date = Date.now();
+  localStorage.setItem(this.nom, JSON.stringify(this));
+ }
+ localStorage.setItem(this.nom, JSON.stringify(this));
 }
-
-var magicarpe = new Pokemon("Magicarpe", "", 129,"male",0,0,0,0, "images/magikarp.gif", dateNav);
-var pikachu = new Pokemon("Pikachu", "", 25, "male", 0,0,0,0, "images/pikachu.gif", dateNav);
-var porygon = new Pokemon("Porygon", "", 137, "male", 0,0,0,0, "images/porygon.gif", dateNav);
-
-document.getElementById("sprite").src = magicarpe.image;
-document.getElementById("Nom").innerHTML = magicarpe.nom ;
-document.getElementById("progress_santé").value = magicarpe.santé
-document.getElementById("progress_humeur").value = magicarpe.humeur
-document.getElementById("progress_faim").value = magicarpe.faim
-document.getElementById("progress_ennui").value = magicarpe.ennui
