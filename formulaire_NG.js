4df5a5703223372044841d00670ed91475584fa0
BobbyCeps <BobbyCeps@gmail.com>
2015-12-22 14:38:30 +0100

    css ng

--- formulaire_NG.js
+++ formulaire_NG.js
@@ -7,11 +7,18 @@
   this.image = "sprite/" + this.nom + ".gif";
   this.date = Date.now();
 }
+/* user */
+function User(nom, mdp, argent) {
+  this.nom = document.getElementById("login_ng").value;
+  this.mdp = document.getElementById("mdp_ng").value;
+  this.argent = argent;
+}
+
 function new_game() {
   document.getElementById("content_log").style.display = "none";
   document.getElementById("content_ng").style.display = "block";
 }
-
+/* fonction de fake connection au "compte" */
 function valider(){
  var login = document.getElementById("login").value;
  var mdp = document.getElementById("mdp").value;
@@ -28,10 +35,14 @@ function valider(){
 function creation(){
   var nom = document.getElementById("login_ng").value;
   var mdp = document.getElementById("mdp_ng").value;
-  var choix_pokemon = document.getElementById("select").value;
-  localStorage[nom]= mdp;
-  var pokemon = new Pokemon("",0,0,0,0,"");
-  localStorage.setItem("Pokemon", JSON.stringify(pokemon));
   document.getElementById("content_log").style.display = "block";
   document.getElementById("content_ng").style.display = "none";
+  var choix_pokemon = document.getElementById("select").value;
+  localStorage[nom]= mdp;
+  /* pokemon + local storage */
+  var pokemon = new Pokemon("",0,0,0,0,"");
+  localStorage.setItem("Pokemon", JSON.stringify(pokemon));
+  /* USER */
+  var user = new User("","",0);
+  localStorage.setItem("User", JSON.stringify(user));
 }
